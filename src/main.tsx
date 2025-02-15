926598f954576c5b027e4f615a376bbc4ee27efe
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-14 22:57:36 -0300

    ✨ feat: integra Apollo Client à renderização da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
 import AppRoutes from '@/routes'
+import ABApolloClient from '@/components/ABApolloClient'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <AppRoutes />
-        </QueryClientProvider>
+        <ABApolloClient>
+            <QueryClientProvider client={queryClient}>
+                <AppRoutes />
+            </QueryClientProvider>
+        </ABApolloClient>
     </React.StrictMode>
 )
